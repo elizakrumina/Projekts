280b3aef5f7186a6ef8ee3ccbd7412939ad36082
elizakrumina <elizakrumina0@gmail.com>
2025-05-25 17:15:46 +0300

    Create datu_apstrade.py

---
+++ datu_apstrade.py
@@ -0,0 +1,9 @@
+
+def ieladet_burtus():
+    return ["a", "p", "e", "s", "l", "u"]
+
+def ieladet_pareizos_vardus():
+    # Šis ir tikai piemērs ar vienkāršiem vārdiem. Reālajā projektā šo sarakstu var paplašināt.
+    return [
+        "pase", "saule", "puse", "apse", "lapsa", "sula", "alus", "elpa", "lupa"
+    ]
